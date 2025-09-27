1ce41de6d67ade0da1c5ef3bba0781a46b450803
berfinkumas <berfinkumas03@gmail.com>
2025-09-27 18:28:10 +0300

    Created tables in db

--- Pycharm/pythonSQL/main.py
+++ Pycharm/pythonSQL/main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 
+
 def createDatabase():
     if os.path.exists("students.db"):
         os.remove("students.db")
@@ -10,9 +11,41 @@ def createDatabase():
     return conn, cursor
 
 
+def createTables(cursor):
+
+    cursor.execute("""
+    CREATE TABLE Students (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        age INTEGER,
+        email VARCHAR UNIQUE,
+        city VARCHAR)
+    """)
+
+    cursor.execute("""
+                   CREATE TABLE Courses 
+                   (
+                       id    INTEGER PRIMARY KEY,
+                       course_name  VARCHAR NOT NULL,
+                       instructor TEXT,
+                       credits  INTEGER
+                   )
+                   """)
+
 
 def main():
     conn, cursor = createDatabase()
+
+    try:
+        createTables(cursor)
+        conn.commit()
+
+    except sqlite3.Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
 
 if __name__ == "__main__":
     main()
